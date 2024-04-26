13b9abec1f387c5793b70c53d6b56e95c0164b4b
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-26 04:25:48 +0200

    add card to layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HeroCardComponent } from './hero-card/hero-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeroCardComponent],
 })
 export class AppComponent {
   title = 'devaka_anagular';
