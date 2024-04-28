aa35e400f5b5e6ab2d16f0f149346553c4501b0f
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-28 21:15:19 +0200

    make cards indicidual

--- src/app/hero-card/hero-card.component.ts
+++ src/app/hero-card/hero-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hero-card',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './hero-card.component.scss',
 })
 export class HeroCardComponent {
-  name = 'Angea';
+  @Input() name: string = '';
+
+  // name = 'Angea';
 }
