3a6ba39df614efd37d8908365d805c87d16764b1
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-28 12:59:31 +0200

    variable replacement

--- src/app/hero-card/hero-card.component.ts
+++ src/app/hero-card/hero-card.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './hero-card.component.html',
-  styleUrl: './hero-card.component.scss'
+  styleUrl: './hero-card.component.scss',
 })
 export class HeroCardComponent {
-
+  name = 'Angea';
 }
