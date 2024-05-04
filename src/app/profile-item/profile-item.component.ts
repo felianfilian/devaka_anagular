9dec0db3664bd9040aa4ce88420dae4c13f363ff
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-04 05:25:27 +0200

    var output testing

--- src/app/profile-item/profile-item.component.ts
+++ src/app/profile-item/profile-item.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-item',
   standalone: true,
   imports: [],
   templateUrl: './profile-item.component.html',
-  styleUrl: './profile-item.component.scss'
+  styleUrl: './profile-item.component.scss',
 })
 export class ProfileItemComponent {
-
+  @Input() name = 'Agnea';
 }
