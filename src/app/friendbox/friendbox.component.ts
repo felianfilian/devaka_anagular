eabcf3e6c06e10180fdd16c2b2d012de62e423b3
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-05-08 11:55:38 +0200

    friends list output

--- src/app/friendbox/friendbox.component.ts
+++ src/app/friendbox/friendbox.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-friendbox',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './friendbox.component.html',
-  styleUrl: './friendbox.component.scss'
+  styleUrl: './friendbox.component.scss',
 })
 export class FriendboxComponent {
-
+  friends = [
+    {
+      name: 'Castii',
+      img: 'assets/img/hero02.jpg',
+      prof: 'Healer',
+    },
+    {
+      name: 'Hikari',
+      img: 'assets/img/hero03.jpg',
+      prof: 'Warrior',
+    },
+    {
+      name: 'Temenos',
+      img: 'assets/img/hero04.jpg',
+      prof: 'Cleric',
+    },
+  ];
 }
